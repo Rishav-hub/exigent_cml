e4dee1dc0094ffed54be5592661050008a4649e3
Ubuntu <ubuntu@ip-10-0-150-34.eu-west-2.compute.internal>
2023-10-20 08:36:18 +0000

    changed configuration

--- config/__init__.py
+++ config/__init__.py
@@ -1,9 +1,9 @@
 
 # Model configuration
-dropdown_checkbox_model_name = "/content/drive/MyDrive/001_projects/exigent/Contract Management and Extraction (CME)/main_data/models/deberta_ck_dd_with_target_cols/model_epoch_4.pt"
+dropdown_checkbox_model_name = "/home/ubuntu/exigent_cml/artifacts/deberta_ck_dd_with_target_cols/model_epoch_4.pt"
 number_model_name = "RishuD7/t5_number_v8"
 options_model_name = "RishuD7/t5_options_v1"
-text_t5_model_name = "/content/drive/MyDrive/001_projects/exigent/Contract Management and Extraction (CME)/main_data/t5_models/text_T5/63.5 Rouge"
+text_t5_model_name = "/home/ubuntu/exigent_cml/artifacts/63.5 Rouge"
 datetime_model_name = "wasimmadha/exigent-datetime-extraction"
 
 # Input data configuration
